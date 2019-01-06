bdf1ca125e67da0f0176374b4a2a12de8d2daf50
Deepak098 <45329357+Deepak098@users.noreply.github.com>
2019-01-06 13:47:48 +0530

    Add files via upload
    new

---
+++ main.js
@@ -0,0 +1,18 @@
+function counterReducer(counter,actionType){
+  if (actionType === "INC"){
+    let v = counter += 1;
+    console.log(`we incremented  the counter to ${v}.`);
+  }
+  else if (actionType === "DEC"){
+    let z = counter -= 1;
+
+    console.log(`We decremented the counter to ${z}.`);
+  }
+
+      else {
+          console.log("Nothing");
+      }
+
+}
+console.log(counterReducer(10,"INC"));
+console.log(counterReducer(18,"DEC"));
